c0f035fe71fd2ee214a1aa2794211c793f4c8865
04sravani <sravaniyedavelli4@gmail.com>
2021-02-23 18:19:57 +0530

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule}   from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, FormsModule,ReactiveFormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
